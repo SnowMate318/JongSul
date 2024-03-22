20778d18533b218bb42fd07db42e275bc7f90ef1
ruluralaa <bsy0302@naver.com>
2024-03-22 20:11:59 +0900

    feat: 문제 생성 페이지 퍼블리싱
    문제 생성 페이지의 퍼블리싱을 진행했습니다.

---
+++ lib/screen/5_generate_problem_screen/generate_problem_complete_screen.dart
@@ -0,0 +1,94 @@
+//문제 생성 완료 페이지
+//완료 버튼 클릭시에 홈으로 이동함
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:jongsul/screen/1_main_screen/home_screen.dart';
+
+class GenerateProblemCompleteScreen extends StatefulWidget {
+  const GenerateProblemCompleteScreen({super.key});
+
+  @override
+  State<GenerateProblemCompleteScreen> createState() =>
+      _GenerateProblemCompleteScreenState();
+}
+
+class _GenerateProblemCompleteScreenState
+    extends State<GenerateProblemCompleteScreen> {
+  TextEditingController multipleChoiceController = TextEditingController();
+  TextEditingController shortAnswerController = TextEditingController();
+  TextEditingController oxQuizController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('문제 생성'),
+        shape: Border(
+          bottom: BorderSide(
+            color: Color(0xFFD5C3B5),
+            width: 1,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(children: [
+          Container(
+            height: 600,
+            child: Center(
+              child: Text(
+                '문제 생성이 완료되었습니다.',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: ElevatedButton(
+                  //icon: Icon(Icons.download, size: 18),
+                  child: Text("홈으로"),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.white,
+                      backgroundColor: Color(0XFF8B5000),
+                      padding:
+                      EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                      textStyle: TextStyle(
+                          fontSize: 14, fontWeight: FontWeight.normal)),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: ElevatedButton(
+                  //icon: Icon(Icons.download, size: 18),
+                  child: Text("문제 풀기"),
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => generate_\()),
+                    // );
+                  },
+                  style: ElevatedButton.styleFrom(
+                      onPrimary: Colors.white,
+                      backgroundColor: Color(0XFF8B5000),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                      textStyle: TextStyle(
+                          fontSize: 14, fontWeight: FontWeight.normal)),
+                ),
+              ),
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
