797695d00c3d29bfecdd3acc4ed411aef35732dd
ruluralaa <bsy0302@naver.com>
2024-04-14 09:58:18 +0900

    feat: 문제 풀이 스크린
    문제 풀이 스크린을 구현했습니다.

---
+++ lib/screen/7_solve_problem_screen/solve_problem_multiple_choice_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:jongsul/screen/7_solve_problem_screen/solve_problem_short_answer_screen.dart';
+import 'package:jongsul/tools/color.dart';
+import 'package:jongsul/screen/widget/menu_bar.dart';
+
+class SolveProblemMultipleChoiceScreen extends StatefulWidget {
+  const SolveProblemMultipleChoiceScreen({super.key});
+
+  @override
+  State<SolveProblemMultipleChoiceScreen> createState() =>
+      _SolveProblemMultipleChoiceScreenState();
+}
+
+class _SolveProblemMultipleChoiceScreenState
+    extends State<SolveProblemMultipleChoiceScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("컴퓨터 네트워크"),
+        shape: const Border(
+          bottom: BorderSide(
+            color: outlineVariant,
+            width: 1,
+          ),
+        ),
+      ),
+      bottomNavigationBar: DownMenuBar(), // 앱바 사용
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '제 1장',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.star),
+                          Text('1'),
+                          Text('/'),
+                          Text('15'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('1. 다음 설명으로 옳은 것을 고르시오'),
+                      SizedBox(height: 50,),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: Text("OUTLINED BUTTON"),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: Text("OUTLINED BUTTON"),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: Text("OUTLINED BUTTON"),
+                      ),
+                      SizedBox(
+                        height: 15,
+                      ),
+                      OutlinedButton(
+                        onPressed: () {},
+                        child: Text("OUTLINED BUTTON"),
+                      ),
+                    ]),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.arrow_back_ios),
+                      IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      SolveProblemShortAnswerScreen()),
+                            );
+                          },
+                          icon: Icon(Icons.arrow_forward_ios)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screen/7_solve_problem_screen/solve_problem_short_answer_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:jongsul/screen/widget/menu_bar.dart';
+import 'package:jongsul/tools/color.dart';
+
+class SolveProblemShortAnswerScreen extends StatefulWidget {
+  const SolveProblemShortAnswerScreen({super.key});
+
+  @override
+  State<SolveProblemShortAnswerScreen> createState() =>
+      _SolveProblemShortAnswerScreenState();
+}
+
+class _SolveProblemShortAnswerScreenState
+    extends State<SolveProblemShortAnswerScreen> {
+  TextEditingController shortAnswerController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("컴퓨터 네트워크"),
+        shape: const Border(
+          bottom: BorderSide(
+            color: outlineVariant,
+            width: 1,
+          ),
+        ),
+      ),
+      bottomNavigationBar: DownMenuBar(), // 앱바 사용
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '제 1장',
+                        style: TextStyle(fontSize: 25),
+                      ),
+                      Row(
+                        children: [
+                          Icon(Icons.star),
+                          Text('1'),
+                          Text('/'),
+                          Text('15'),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('1. 다음중 참 거짓을 판별하시오'),
+                      SizedBox(height: 50),
+                    ],
+                  ),
+                ),
+
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text('정답:', style: TextStyle(fontSize: 20),),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Container(
+                    width: 200,
+                    height: 35,
+                    child: TextField(
+                      maxLength: 2,
+                      controller: shortAnswerController,
+                      textAlignVertical: TextAlignVertical.top,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelStyle: TextStyle(color: Color(0xFF8B5000)),
+                        counterText: '', //글자수 제한 표시 없앰
+                      ),
+                    ),
+                  ),
+                ]),
+                SizedBox(height: 60,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    OutlinedButton(
+                      onPressed: () {},
+                      child: Text("제출하기"),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.arrow_back_ios),
+                      IconButton(
+                          onPressed: () {
+                            // Navigator.push(
+                            //   context,
+                            //   MaterialPageRoute(
+                            //       builder: (context) =>
+                            //           SolveProblemMultipleChoiceScreen()),
+                            // );
+                          },
+                          icon: Icon(Icons.arrow_forward_ios)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
