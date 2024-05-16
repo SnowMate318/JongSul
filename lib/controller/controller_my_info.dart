7151ababc4debf381c83fbfd0125418f88c23eb7
ruluralaa <bsy0302@naver.com>
2024-05-16 21:21:33 +0900

    feat: 내정보 컨트롤러 생성
    내정보 컨트롤러 생성

--- lib/controller/controller_my_info.dart
+++ lib/controller/controller_my_info.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:jongsul/models/user/service_user.dart';
 import 'package:jongsul/models/user/user_data.dart';
+
 class MyInfo extends GetxController{
   ServiceUser _serviceUser = ServiceUser.init();
   ServiceUser get serviceUser => _serviceUser;
@@ -11,5 +12,19 @@ class MyInfo extends GetxController{
     update();
     return _serviceUser;
   }
+
+  Future<bool> setMyInfo(ServiceUser user) async {
+
+    bool ret = await setServiceUser(user.toJson());
+    update();
+    return ret;
+  }
+
+//   Future<bool> removeStudent(String id) async {
+//
+//     // bool ret = await deleteServiceUser(id);
+//     // update();
+//     return ret;
+// }
 
 }
